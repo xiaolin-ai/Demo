665147c7a72c03a08f189304db1b3e80f64d90a3
likedust@126.com <${likedust@126.com}>
2020-04-25 20:59:02 +0800

    第一次测试

---
+++ demo/src/main/java/org/example/demo/Main.java
@@ -0,0 +1,12 @@
+package org.example.demo;
+
+/**
+ * @Author: aixiaolin
+ * @Date: 2020-04-25 20:54
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world !");
+    }
+}

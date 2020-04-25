cb8693f9f3db7e3d53e4e019702358b2caecf11a
likedust@126.com <${likedust@126.com}>
2020-04-25 22:30:40 +0800

    master主分支第五次提交

---
+++ demo/src/main/java/org/example/demo/Test2.java
@@ -0,0 +1,12 @@
+package org.example.demo;
+
+/**
+ * @Author: aixiaolin
+ * @Date: 2020-04-25 22:29
+ * @Description:
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("hello !");
+    }
+}
